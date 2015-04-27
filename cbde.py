7d1163318ddf90fc8cdc14d755ff9f76e3f85ac6
alexmorral <alexmorral@gmail.com>
2015-04-27 20:59:59 +0200

    First commit with simple example

---
+++ cbde.py
@@ -0,0 +1,14 @@
+/*
+mongod //starts mongoDB
+mongoimport --db databaseName --collection collectionName --drop --file file.json
+Example:
+mongoimport --db test --collection restaurants --drop --file primer-dataset.json
+*/
+
+from pymongo import MongoClient
+client = MongoClient()
+db = client.test
+cursor = db.restaurants.find({"borough": "Manhattan"})
+
+for document in cursor:
+    print(document)
